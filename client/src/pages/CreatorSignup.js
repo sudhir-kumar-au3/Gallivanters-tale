04b8df9e63c4036747abcb44bed71e7e918054da
Sudhir Kumar <sudhir.kr23.1995@gmail.com>
2020-02-05 11:08:41 +0530

    bolilerplate for project

---
+++ client/src/pages/CreatorSignup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignupForm from '../components/SignupForm'
+function CreatorSignup() {
+    return (
+        <div>
+            <SignupForm></SignupForm>
+        </div>
+    )
+}
+
+export default CreatorSignup
